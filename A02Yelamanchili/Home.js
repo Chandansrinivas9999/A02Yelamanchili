72245bd95f650dd76a87bb11478bc56d939275ae
Chandansrinivas9999 <S528185@mail.nwmissouri.edu>
2016-09-23 23:39:54 -0500

    Add files via upload

---
+++ A02Yelamanchili/Home.js
@@ -0,0 +1,8 @@
+
+function multiplyBy(){
+var myBox1 = $('#t').val();
+var myBox2 = document.getElementById('p').value;
+var cost = 60;
+var cp = 20;
+ document.getElementById("result").innerHTML=parseFloat((myBox1)*cost + myBox2*cp);
+};
